e148bdea900a5d3d0848cc84072d8644a410d2d6
benaiah-ke <benaiahpraise@gmail.com>
2022-11-27 12:45:32 +0300

    completed lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+    if array.length > 2
+        last = array[-1]
+        array.pop
+
+        string = array.join(", ")
+        string << ", and #{last}"
+
+        string
+    elsif array.length == 2 
+        array.join(" and ")
+    else
+        array.join
+    end
+
 end
+
+p oxford_comma(["Kenya","Uganda","Tanzania"])
